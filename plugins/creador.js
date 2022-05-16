0b4b1edeb14b84f9565fa9ad5c6dba8d7edec572
Yisus1587 <105611431+Yisus1587@users.noreply.github.com>
2022-05-16 02:13:25 -0500

    Create creador.js

---
+++ plugins/creador.js
@@ -0,0 +1,19 @@
+function handler(m) {
+  /*const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)*/
+  
+  m.reply(`
+*≡ Crédito*
+  *YISUS1587*
+`) 
+
+
+}
+
+handler.help = ['creador']
+handler.tags = ['main']
+
+handler.command = ['owner', 'creator', 'creador', 'dueño', 'fgowner'] 
+
+
+export default handler
