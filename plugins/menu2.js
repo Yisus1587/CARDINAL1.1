5d21dd9d897774c256b853df0c3c97fba7946d4b
Yisus1587 <105611431+Yisus1587@users.noreply.github.com>
2022-06-01 15:09:36 -0500

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -5,9 +5,7 @@ let handler = async function (m, { conn, text, usedPrefix }) {
 let m2 = `
 ≡ _Use estos comandos sin el prefijo_
 
-▢ Si tienes más  audios  
-envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
-*No* _voces de lolis_
+
 
 
 ┌─⊷ *AUDIOS* 
@@ -19,9 +17,9 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 ┌─⊷ *VIDEO/GIF* 
 ▢ Linda noche
 └──────────────`
-const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
+const pp = await (await fetch('https://i.ibb.co/b2k64vJ/avatar-contact.jpg')).buffer()
     
-    conn.sendHydrated(m.chat, m2, '▢ CARDINAL  ┃ BOT\n▢ \n', pp, '', '', null, null, [
+    conn.sendHydrated(m.chat, m2, '▢ CARDINAL  ┃ BOT\n▢', pp, '', '', null, null, [
      ['⏍ Info', '/botinfo'],
  
     ], m)
